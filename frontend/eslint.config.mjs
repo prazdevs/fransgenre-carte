836cc663b76f50d0531fb91e69941bfa8dc0d985
Chloe B <hellochloe1989@protonmail.com>
2025-10-30 02:21:04 +0100

    eslint : vue/max-attributes-per-line erreur plutôt que warning

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,5 +3,13 @@ import stylistic from '@stylistic/eslint-plugin'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
+
   stylistic.configs['recommended'],
+
+  {
+    rules: {
+      'vue/max-attributes-per-line': ['error'],
+    },
+  },
+
 )
